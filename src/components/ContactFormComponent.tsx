5b1812a41535d80693e3101030a0500761f01b64
goodfoodtruck <mielatomique@protonmail.com>
2024-07-21 21:53:44 +0200

    update contactform

--- src/components/ContactFormComponent.tsx
+++ src/components/ContactFormComponent.tsx
@@ -1,25 +1,22 @@
-import { FormEvent, useState } from "react"
+import { useContext, useState } from "react"
+import { ThemeContext } from "../contexts/ThemeContext"
 
 export const ContactFormComponent = () => {
+    const { theme } = useContext(ThemeContext)
     const [fullname, setFullname] = useState("")
     const [mail, setMail] = useState("")
     const [content, setContent] = useState("")
-    const userBody = JSON.stringify({fullname, mail, content})
-
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        console.log("Formulaire rempli")
-    }
+    const [sent, setSent] = useState(false)
 
     return (
-        <div className="m-4">
+        <div className={`p-52 text-${theme}`}>
             <h2 className="mb-8 text-3xl text-center font-sans font-bold">Formulaire de contact</h2>
-            <form onSubmit={handleSubmit} className="flex flex-col p-8 rounded-lg">
+            <div className="flex flex-col p-8 rounded-lg text-black">
                 <input
                     type="text"
                     onChange={e => setFullname(e.target.value)}
                     value={fullname}
-                    className="w-64 md:w-80 m-2 p-2 md:p-1 rounded-md font-sans"
+                    className="w-64 m-2 p-2 md:p-1 rounded-md font-sans"
                     name="fullname"
                     placeholder="Votre nom"
                 />
@@ -27,24 +24,24 @@ export const ContactFormComponent = () => {
                     type="mail"
                     value={mail}
                     onChange={e => setMail(e.target.value)}
-                    className="w-64 md:w-80 m-2 p-2 md:p-1 rounded-md font-sans"
+                    className="w-64 m-2 p-2 md:p-1 rounded-md font-sans"
                     name="mail"
                     placeholder="Email"
                 />
-                <input
-                    type="text"
+                <textarea
                     value={content}
                     onChange={e => setContent(e.target.value)}
-                    className="w-64 md:w-80 m-2 p-2 md:p-1 rounded-md font-sans"
+                    className="md:w-80 m-2 p-2 md:p-1 rounded-md font-sans"
                     name="content"
                     placeholder="Votre message..."
                 />
-                <input
-                    type="submit"
-                    id="button"
-                    className="mt-4 mx-auto md:p-2 font-sans cursor-pointer"
-                />
-            </form>
+                <button
+                    onClick={() => setSent(true)}
+                    className={`btn ${sent ? "bg-gray-300" : "bg-blue-500 hover:bg-blue-700"} font-bold py-2 px-4 rounded w-full`}
+                    disabled={sent}>
+                    Envoyer
+                </button>
+            </div>
         </div>
     )
 }
